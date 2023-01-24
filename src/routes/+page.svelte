ee079f3494ac6e9511ef40088eafaa21faca48c9
Rodney Lab <codesign@rodneylab.com>
2023-01-24 17:47:30 +0000

    fix: 💫 update form

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,12 +7,13 @@
 	</ul>
 
 	<h2>Contact us</h2>
-	<form method="post" action="/&message-submitted=true">
+	<form method="post" action="/?message-submitted=true">
 		<label for="name">Name</label>
 		<input id="name" type="text" placeholder="Name" />
 		<label for="email">Email</label>
 		<input id="email" type="text" placeholder="Email" />
 		<input type="hidden" name="inbox_key" value="contact-form" />
 		<textarea id="message" placeholder="Type your message here" />
+		<button type="submit">Send</button>
 	</form>
 </main>
